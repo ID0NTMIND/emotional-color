dca2f9d0194fffa566f3918d36f21dd722bcc8ba
Dmitriy Smalev <n0thingss@yandex.ru>
2026-08-06 15:26:36 +0300

    update | main

--- app/src/main.py
+++ app/src/main.py
@@ -1,14 +1,21 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from src.db.init_db import init
+from src.api import auth, users, balance, history, predictions
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    init()   # выполняется при запуске
-    yield    # приложение работает
+    init()
+    yield
 
 app = FastAPI(title="ML Service API", lifespan=lifespan)
+
+app.include_router(auth.router)
+app.include_router(users.router)
+app.include_router(balance.router)
+app.include_router(history.router)
+app.include_router(predictions.router)
 
 
 @app.get("/")
